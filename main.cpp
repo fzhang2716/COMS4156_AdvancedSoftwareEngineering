d1c31510edff1aa09717ed2a664244dafca21e58
lhalice <lhalice@umich.edu>
2023-10-14 16:50:45 -0400

    test vm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 // Sets up the server
 #include<iostream>
 #include "data_management.hpp"
-//#include "crow.h"
+#include "crow.h"
 
 int main() {
     DataManagementService datamanage;
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
     return 0;
 }
