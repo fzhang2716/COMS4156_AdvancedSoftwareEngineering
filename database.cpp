b0ec3c9751b67de23ac8d108e57590cdeb625ff3
lhalice <lhalice@umich.edu>
2023-10-17 20:18:54 -0400

    add get company info request, debugging

--- database.cpp
+++ database.cpp
@@ -8,25 +8,76 @@
 #include <mysql_driver.h>
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
+#include <cppconn/connection.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
-
-std::string GetTestTable(){
+sql::Connection* DBConnect(){
     // database connection
     sql::mysql::MySQL_Driver* driver;
-    sql::Connection* con;
+    sql::Connection* conn;
 
     driver = sql::mysql::get_mysql_driver_instance();
     // connect IP adress, username, password
-    con = driver->connect("tcp://34.150.169.58", "admin", "debugteam"); 
+    conn = driver->connect("tcp://34.150.169.58", "admin", "debugteam"); 
 
+    return conn;
+}
+
+void DBDisConnect(sql::Connection* conn){
+    delete conn;
+}
+
+/**
+ * If user is authorized, return its company_id
+ * otherwise, return -1
+*/
+int isUserAuthenticated(const crow::request& req, crow::response& res, sql::Connection* conn) {
+    // Extract username and password from the request.
+    std::string username = req.url_params.get("username");
+    std::string password = req.url_params.get("password");
+
+
+    // Query your MySQL database to validate the credentials.
+    // If the credentials are valid, return true; otherwise, return false.
+
+    // Example of a query to check user credentials:
+    try{
+        sql::Statement* stmt = conn->createStatement();
+        sql::ResultSet* result = stmt->executeQuery("SELECT * FROM service.company_table WHERE company_name = '" + username + "' AND hash_pwd = '" + password + "'");
+
+        if (result->next()) {
+            int companyId = result->getInt("company_id");
+            std::cout << "Authorized company id is " << companyId << std::endl;
+            delete result;
+            delete stmt;
+            res.code = 200;
+            res.write("Authentication success");
+            return companyId;
+        }
+
+        // If credentials are not valid, return false and deny access.
+        delete result;
+        delete stmt;
+        res.code = 401; // Unauthorized
+        res.write("Authentication failed");
+        return -1;
+    } catch (sql::SQLException& e ) {
+        res.code = 500;
+        res.write("Database Error: " + std::string(e.what()));
+    }
+
+    return -1;
+}
+
+std::string GetTestTable(sql::Connection* conn){
     sql::Statement* stmt;
     sql::ResultSet* res;
 
     std::string data = "";
-    stmt = con->createStatement();
+    stmt = conn->createStatement();
     res = stmt->executeQuery("SELECT * FROM test_schema.test_user_tbl");
 
     while(res->next()){
@@ -39,8 +90,6 @@ std::string GetTestTable(){
 
     delete res;
     delete stmt;
-
-    delete con;
 
     return data;
 }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
 {
     crow::SimpleApp app;
 
+
     CROW_ROUTE(app, "/test")
     ([]()
      {
@@ -15,8 +16,55 @@ int main()
     CROW_ROUTE(app, "/json")
     ([]
      {
-    crow::json::wvalue x({{"message", GetTestTable()}});
+    sql::Connection* conn = DBConnect();
+    crow::json::wvalue x({{"message", GetTestTable(conn)}});
+    DBDisConnect(conn);
     return x; });
 
+
+    /**
+     * Get a company's information
+     * http://localhost:3000/company?username=company1&password=pwd
+    */
+    CROW_ROUTE(app, "/company")
+        .methods(crow::HTTPMethod::GET)
+    ([&] (const crow::request& req, crow::response& res)
+    {
+        sql::Connection* conn = DBConnect();
+        int companyId = isUserAuthenticated(req, res, conn);
+
+        std::cout << "company Id is: " << companyId << std::endl;
+        if(companyId != -1){
+            sql::PreparedStatement* prep_stmt = conn->prepareStatement("SELECT * FROM service.company_table WHERE company_id = ?");
+            prep_stmt->setInt(1,companyId);
+
+            try{
+                sql::ResultSet* queryResult = prep_stmt->executeQuery();
+                if(queryResult->rowsCount() > 0){
+                    std::string companyData= ""; 
+                    while( queryResult->next()){
+                        std::cout << "queryFounded";
+                        companyData += queryResult->getInt("company_id");
+                        //companyData += queryResult->getString("company_name") + ", ";
+                        //companyData += queryResult->getString("email") + " \n";
+                    }
+
+                    std::cout << "print company data: " << companyData;
+                    res.code = 200;
+                    res.write(companyData);
+                }else{
+                    std::cout << "noqueryFound" << std::endl;
+                }
+                
+                delete queryResult;
+                delete prep_stmt;
+
+            } catch (const sql::SQLException& e){
+                std::cerr << "SQL ERROR: " << e.what() << std::endl;
+            }
+        }
+        DBDisConnect(conn);
+    });
+    
     app.port(3000).multithreaded().run();
 }
