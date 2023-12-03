11559dc768d364d3dc9831a99fdd6118d065c109
lhalice <lhalice@umich.edu>
2023-12-02 22:49:04 -0500

    fix integration test bug

--- integration_test.cpp
+++ integration_test.cpp
@@ -14,52 +14,68 @@
 
 DataManagementService dataservice;
 
-std::string jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwMzA2ODgsImlzcyI6IlN1Yk1hbmFnZXIiLCJzdWIiOiIyMCJ9.DZGXi-CvQeQOhwK9HB4FYWZMjZr-J914FT5_UaFC_44";  //JWT token （hardcode an example for demo）
-httplib::Headers headers = {
-    {"Authorization", "Bearer " + jwtToken}
+/**
+ * This class is used for test setup
+*/
+class ServerFixture {
+public:
+    httplib::Headers headers;
+   
+    ServerFixture() { 
+        headers.emplace("Authorization", jwtToken);
+        //std::cout << "ServerFixture constructor called" << std::endl;
+        startServer();
+        // Optionally: Wait for the server to be ready before proceeding
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    ~ServerFixture() {
+        //std::cout << "ServerFixture destructor called" << std::endl; 
+        stopServer();
+    }
+    
+
+    void startServer() {
+        //std::cout << "startServer called" << std::endl; 
+        serverThread = std::thread([this]() {
+            app.port(8080).multithreaded().run();
+            serverRunning = false;
+        });
+        serverRunning = true;
+        serverThread.detach();
+    }
+
+    void stopServer() {
+        //std::cout << "stopServer called" << std::endl;
+        app.stop();
+        if (serverRunning) {
+            serverRunning = false;
+        }
+    }
+
+private:
+    crow::SimpleApp app;
+    std::atomic<bool> serverRunning{false};
+    std::thread serverThread;
+    std::string jwtToken ="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwMzA2ODgsImlzcyI6IlN1Yk1hbmFnZXIiLCJzdWIiOiIyMCJ9.DZGXi-CvQeQOhwK9HB4FYWZMjZr-J914FT5_UaFC_44";
 };
 
-crow::SimpleApp app;
-
-std::atomic<bool> serverRunning(false);
-std::thread serverThread([]() {
-    app.port(8080).run();
-    serverRunning = false;
-});
-
-void startServer() {
-    serverRunning = true;
-    serverThread.detach();
-}
-
-void stopServer() {
-    if (serverRunning) {
-        serverRunning = false;
-    }
-}
-
-TEST_CASE("/company test", "[routes]" ) {
-    startServer();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+TEST_CASE_METHOD(ServerFixture, "/company test", "[routes][company]") {
+    //std::cout << "Test running" << std::endl;
     httplib::Client client("localhost", 3000);
 
     auto response = client.Get("/company", headers);
     REQUIRE(response->status == 200);
-    //std::string response1 = response->body;
-    //response1.erase(std::remove(response1.begin(), response1.end(), '\n'), response1.end());
-    
+
     Json::Value targetJson;
     targetJson["company_id"] = "20";
     targetJson["company_name"] = "frank";
     targetJson["email"] = "hz2716@columbia.edu";
-    
-    // Parse request body as JSON
+
     Json::CharReaderBuilder reader;
     Json::Value responseJson;
     std::istringstream responseBody(response->body);
     Json::parseFromStream(reader, responseBody, &responseJson, nullptr);
 
-    // Compare the two JSON objects
     REQUIRE(responseJson == targetJson);
-    stopServer();
 }
